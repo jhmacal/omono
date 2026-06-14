c74b626d2947dbe4a7d00c35b492e115b14455dc
J.H <macal@MacBook-Pro.local>
2026-06-13 20:55:47 -0400

    Refresh O'Mono app icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "omono-v2";
+const CACHE = "omono-v3";
 const ASSETS = ["./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
